d3b664e8b634d8c073a8494cdf9b891df7cd9008
Dmitry Kouznetsov <dmitry.kouznetsov@viaplay.com>
2017-03-21 14:41:21 +0100

    AsyncStorage

--- src/index.ios.tsx
+++ src/index.ios.tsx
@@ -1,9 +1,9 @@
-import { AppRegistry, ListView, Text, View } from 'react-native'
+import { AppRegistry, ListView, Text, View, AsyncStorage } from 'react-native'
 
 import React from 'react'
 
 import { setObservableConfig, mapPropsStream } from 'recompose'
-import { periodic, from, fromPromise } from 'most'
+import { just, periodic, from, fromPromise, throwError } from 'most'
 // import { async, Subject } from 'most-subject'
 import * as R from 'ramda'
 
@@ -11,17 +11,28 @@ setObservableConfig({
   fromESObservable: from as any
 })
 
-const name$ = fromPromise(
+const KEY: string = '@MySuperStore:names'
+
+const storedUser$ = fromPromise(
+  AsyncStorage.getItem(KEY)
+).flatMap(x =>
+  (x)
+    ? just(JSON.parse(x))
+    : throwError(new Error('nothing was found in storage'))
+).tap(() => console.log('loading from local storage'))
+.flatMap(x => from(x as any))
+
+const fetchedUser$ = fromPromise(
   fetch('https://jsonplaceholder.typicode.com/users')
   .then(response => response.json())
+).tap(() => console.log('fetching')).tap(users =>
+  AsyncStorage.setItem(KEY, JSON.stringify(users))
 ).flatMap(x => from(x as any))
-.map(R.prop('name'));
+
+const name$ = storedUser$.recoverWith(() => fetchedUser$).map(R.prop('name'))
 
 const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2})
 
-// const data$ = from([
-//   'John', 'Joel', 'James', 'Jimmy', 'Jackson', 'Jillian', 'Julie', 'Devin'
-// ])
 const data$ = name$
 .zip(R.identity, periodic(500))
 .scan((acc, x) => R.concat(acc, R.of(x)), [])
