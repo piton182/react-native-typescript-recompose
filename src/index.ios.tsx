a1f985631c9dede69dccf66c678c9851b55c2f78
Dmitry Kouznetsov <dmitry.kouznetsov@viaplay.com>
2017-03-21 13:44:34 +0100

    add networking

--- src/index.ios.tsx
+++ src/index.ios.tsx
@@ -3,7 +3,7 @@ import { AppRegistry, ListView, Text, View } from 'react-native'
 import React from 'react'
 
 import { setObservableConfig, mapPropsStream } from 'recompose'
-import { periodic, from } from 'most'
+import { periodic, from, fromPromise } from 'most'
 // import { async, Subject } from 'most-subject'
 import * as R from 'ramda'
 
@@ -11,13 +11,20 @@ setObservableConfig({
   fromESObservable: from as any
 })
 
+const name$ = fromPromise(
+  fetch('https://jsonplaceholder.typicode.com/users')
+  .then(response => response.json())
+).flatMap(x => from(x as any))
+.map(R.prop('name'));
+
 const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2})
 
-const data$ = from([
-  'John', 'Joel', 'James', 'Jimmy', 'Jackson', 'Jillian', 'Julie', 'Devin'
-])
+// const data$ = from([
+//   'John', 'Joel', 'James', 'Jimmy', 'Jackson', 'Jillian', 'Julie', 'Devin'
+// ])
+const data$ = name$
 .zip(R.identity, periodic(500))
-.scan((acc, x) => R.concat(R.of(x), acc), [])
+.scan((acc, x) => R.concat(acc, R.of(x)), [])
 
 const MyListViewBasics = dataBlob =>
   <View style={{flex: 1, paddingTop: 22}}>
