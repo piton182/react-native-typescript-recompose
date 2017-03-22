773f24222ca7017992eb0760d7bd502df471a457
Dmitry Kouznetsov <dmitry.kouznetsov@viaplay.com>
2017-03-22 11:39:26 +0100

    logo screen -> auth0 lock

--- src/index.ios.tsx
+++ src/index.ios.tsx
@@ -1,50 +1,35 @@
-import { AppRegistry, ListView, Text, View, AsyncStorage } from 'react-native'
+import { AppRegistry, Text, View } from 'react-native'
 
 import React from 'react'
 
-import { setObservableConfig, mapPropsStream } from 'recompose'
-import { just, periodic, from, fromPromise, throwError } from 'most'
-// import { async, Subject } from 'most-subject'
+import { mapPropsStream } from 'recompose'
 import * as R from 'ramda'
 
-setObservableConfig({
-  fromESObservable: from as any
-})
+import Auth0Lock from 'react-native-lock'
+import credentials from './.auth0-credentials'
+const lock = new Auth0Lock(credentials)
 
-const KEY: string = '@MySuperStore:names'
+setTimeout(
+  () => lock.show({
+    closable: false,
+  }, (err, profile, token) => {
+    if (err) {
+      console.log(err)
+    } else {
+      console.log('Logged in!')
+    }
+  })
+  , 1000
+)
 
-const storedUser$ = fromPromise(
-  AsyncStorage.getItem(KEY)
-).flatMap(x =>
-  (x)
-    ? just(JSON.parse(x))
-    : throwError(new Error('nothing was found in storage'))
-).tap(() => console.log('loading from local storage'))
-.flatMap(x => from(x as any))
-
-const fetchedUser$ = fromPromise(
-  fetch('https://jsonplaceholder.typicode.com/users')
-  .then(response => response.json())
-).tap(() => console.log('fetching')).tap(users =>
-  AsyncStorage.setItem(KEY, JSON.stringify(users))
-).flatMap(x => from(x as any))
-
-const name$ = storedUser$.recoverWith(() => fetchedUser$).map(R.prop('name'))
-
-const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2})
-
-const data$ = name$
-.zip(R.identity, periodic(500))
-.scan((acc, x) => R.concat(acc, R.of(x)), [])
-
-const MyListViewBasics = dataBlob =>
-  <View style={{flex: 1, paddingTop: 22}}>
-    <ListView
-      dataSource={ds.cloneWithRows(dataBlob)}
-      renderRow={(rowData) => <Text>{rowData}</Text>}
-    />
+const AppHOC = () =>
+  <View style={{
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  }}>
+    <Text style={{fontSize: 30}}>Logo</Text>
   </View>
+const App = mapPropsStream(R.identity)(AppHOC)
 
-const ListViewBasics = mapPropsStream(() => data$)(MyListViewBasics)
-
-AppRegistry.registerComponent('ListViewBasics', () => ListViewBasics)
+AppRegistry.registerComponent('App', () => App)
